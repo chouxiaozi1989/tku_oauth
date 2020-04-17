b5a650d034683ddf735fcbc9712fbb0f4a7a3edc
hushungang <hsgang@dareway.com.cn>
2020-04-17 14:51:20 +0800

    添加  界面处理映射器

--- src/main/java/com/tku/tku_oauth/oauth/config/InterceptorConfig.java
+++ src/main/java/com/tku/tku_oauth/oauth/config/InterceptorConfig.java
@@ -4,6 +4,7 @@ import com.tku.tku_oauth.oauth.interceptors.TokenInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
@@ -21,5 +22,16 @@ public class InterceptorConfig extends WebMvcConfigurationSupport {
                 .addPathPatterns("/api/**")
                 .addPathPatterns(logPathPatterns)
                 .excludePathPatterns(excludePathPatterns);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations(
+                "classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations(
+                "classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations(
+                "classpath:/META-INF/resources/webjars/");
+        super.addResourceHandlers(registry);
     }
 }
